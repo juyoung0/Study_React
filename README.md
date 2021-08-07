3af1370811d79d60e6e1886567d7ff6fb3509f68
Juyoung Oh <juyoung.oh@hyundai.com>
2021-08-07 16:25:53 +0900

    Fix: Remove minor bug

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 리액트 공부를 위해 영화 앱을 만들었습니다.
+
+
+참고 : https://nomadcoders.co/react-fundamentals/lobby
--- src/App.js
+++ src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
   }
 
 
-	
+
   _renderMovies = () => {
 	const movies = this.state.movies.map(movie => {
                 return <Movie title={movie.title}
-			poster={movie.medium_cover_image} 
-			genres={movie.genres} 
+			poster={movie.medium_cover_image}
+			genres={movie.genres}
 			synopsis = {movie.synopsis}
 			key={movie.id}/>
         })
@@ -33,7 +33,7 @@ class App extends Component {
   }
 
   _callApi = () => {
-        return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+        return fetch('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
         .then(response => response.json())
         .then(json => json.data.movies)
         .catch(err => console.log(err))
